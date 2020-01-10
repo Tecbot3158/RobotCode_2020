f8db55c0bfe544627cf3cd37741fe3f442984d23
Alex <alex3157@yahoo.com>
2020-01-10 17:54:31 -0600

    Translated code

---
+++ src/main/java/frc/robot/splines/Derivative.java
@@ -0,0 +1,16 @@
+package frc.robot.splines;
+
+public class Derivative {
+	
+	 public double squareX;
+     public double x;
+     public double indep;
+
+     public Derivative(double square, double lineal, double term)
+     {
+         squareX = square;
+         x = lineal;
+         indep = term;
+     }
+
+}
