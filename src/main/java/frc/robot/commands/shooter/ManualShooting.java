fde7102ff7f61afffccda5d2d6afb28cea31798c
Gabo1245 <ponce,pgabriel@gmail.com>
2020-02-01 15:41:29 -0600

    A Instand Command for using the shooter manually

---
+++ src/main/java/frc/robot/commands/shooter/ManualShooting.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Robot;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class ManualShooting extends InstantCommand {
+  double manualspeed; 
+  double manualangle;
+  public ManualShooting(double ManualSpeed, double ManualAngle) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.getRobotContainer().getShooter());
+     
+    manualspeed = ManualSpeed; 
+    manualangle = ManualAngle;
+      
+    
+  }
+ 
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize()
+   {
+    Robot.getRobotContainer().getShooter().setManualShooter(manualspeed);
+    Robot.getRobotContainer().getShooter().setManualShooter(manualangle);
+    }
+
+  }
