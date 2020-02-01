1816396bfc7bc854b54260567d4fc6e12c7daf8e
TrevCan <heror0484@gmail.com>
2020-01-31 18:53:39 -0600

    Created TecbotMotorList.java and RobotConfigurator.buildMotorList

---
+++ src/main/java/frc/robot/commands/robotActions/RA_ALL_SYSTEMS_OFF.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.robotActions;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class RA_ALL_SYSTEMS_OFF extends SequentialCommandGroup {
+    /**
+     * <h3><strong>ALL SYSTEMS OFF</strong></h3>
+     * <ul>
+     *
+     * <li>Intakes:
+     * <ul>
+     * <li>Front intake on off mode, pneumatics off</li>
+     * <li>Rear intake on off mode, pneumatics off</li>
+     * </ul>
+     * </li>
+     *
+     * <li>Power Cell Transportation System
+     * <ul>
+     * <li>Off mode, deflector off</li>
+     * </ul>
+     * </li>
+     *
+     * <li>Powercell shooter:
+     * <ul>
+     * <li>On position #OFF</li>
+     * </ul>
+     * </li>
+     *
+     * </ul>
+     */
+    public RA_ALL_SYSTEMS_OFF() {
+        // Add your commands in the super() call, e.g.
+        // super(new FooCommand(), new BarCommand());
+        super();
+        addCommands(
+                /*
+                FI PNEUMATICS OFF
+                FI OFF MODE
+                RI PNEUMATICS OFF
+                RI OFF MODE
+                PCTS OFF, DEF OFF
+                PCS OFF
+                 */
+        );
+    }
+}
